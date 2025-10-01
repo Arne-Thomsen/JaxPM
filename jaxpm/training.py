6a09618930e43d120e8a09a4d5956f252e89f67f
Arne Thomsen <athomsen@student.ethz.ch>
2025-10-01 05:37:48 -0700

    put training functions into .py from .ipynb

--- jaxpm/training.py
+++ jaxpm/training.py
@@ -3,9 +3,8 @@ import jax.numpy as jnp
 
 from diffrax import diffeqsolve, ODETerm, LeapfrogMidpoint, SaveAt, ConstantStepSize, StepTo
 from flax import nnx
-import orbax.checkpoint as ocp
 
-from jaxpm import hpm, utils
+from jaxpm import hpm, utils, augmentations
 
 
 def get_ode_solver(mesh_per_dim, cosmo, max_steps=1000):
@@ -56,21 +55,112 @@ def get_ode_solver(mesh_per_dim, cosmo, max_steps=1000):
     return solve_ode
 
 
-# TODO integrate into network classes
-def load_checkpoint(model, checkpoint_file):
-    abstract_model = nnx.eval_shape(lambda: model)
-    graphdef, abstract_params = nnx.split(abstract_model)
+def train_step_base(model, optimizer, loss_fn):
+    loss, grads = nnx.value_and_grad(loss_fn)(model)
+    optimizer.update(grads)
 
-    checkpointer = ocp.StandardCheckpointer()
-    params = checkpointer.restore(checkpoint_file, abstract_params)
-    model = nnx.merge(graphdef, params)
-    print(f"Checkpoint loaded from {checkpoint_file}")
+    squared_sum = jax.tree_util.tree_reduce(lambda x, y: x + jnp.sum(y**2), grads, 0.0)
+    grad_norm = jnp.sqrt(squared_sum)
 
-    return model
+    return loss, grad_norm
 
 
-def save_checkpoint(model, checkpoint_file):
-    _, params = nnx.split(model)
-    checkpointer = ocp.StandardCheckpointer()
-    checkpointer.save(checkpoint_file, params, force=True)
-    print(f"Checkpoint saved to {checkpoint_file}")
+def get_train_step(mesh_per_dim, cosmo, max_steps=1000):
+    solve_ode = get_ode_solver(mesh_per_dim, cosmo, max_steps=max_steps)
+
+    def model_loss_base_fn(
+        loss_fn,
+        y0,
+        t0,
+        ref_t,
+        ref_poss,
+        ref_vels=None,
+        ref_cls=None,
+        ref_deltas=None,
+        gravity_model=None,
+        pressure_model=None,
+        species="gas",
+        tstep=None,
+        nt=2,
+    ):
+        # integrate
+        res = solve_ode(y0, t0, ref_t, gravity_model, pressure_model, tstep=tstep, nt=nt)
+        if species == "dm":
+            res_poss, res_vels = res[0], res[1]
+        elif species == "gas":
+            res_poss, res_vels = res[2], res[3]
+        else:
+            raise NotImplementedError
+
+        return loss_fn(
+            res_poss=res_poss,
+            res_vels=res_vels,
+            ref_poss=ref_poss,
+            ref_vels=ref_vels,
+            ref_cls=ref_cls,
+            ref_deltas=ref_deltas,
+        )
+
+    @nnx.jit(static_argnames=("loss_fn", "model_to_train", "nt"))
+    def train_step(
+        loss_fn,
+        optimizer,
+        y0,
+        t0,
+        ref_t,
+        ref_poss,
+        ref_vels=None,
+        ref_cls=None,
+        ref_deltas=None,
+        gravity_model=None,
+        pressure_model=None,
+        tstep=None,
+        # static
+        nt=2,
+        model_to_train="pressure",
+    ):
+        """dynamic snapshot range as passed"""
+
+        if model_to_train == "pressure":
+
+            def model_loss_fn(model):
+                return model_loss_base_fn(
+                    loss_fn,
+                    y0,
+                    t0,
+                    ref_t,
+                    ref_poss,
+                    ref_vels,
+                    ref_cls,
+                    ref_deltas,
+                    gravity_model=gravity_model,
+                    pressure_model=model,
+                    species="gas",
+                    tstep=tstep,
+                    nt=nt,
+                )
+
+            return train_step_base(pressure_model, optimizer, model_loss_fn)
+
+        elif model_to_train == "gravity":
+
+            def model_loss_fn(model):
+                return model_loss_base_fn(
+                    loss_fn,
+                    y0,
+                    t0,
+                    ref_t,
+                    ref_poss,
+                    ref_vels,
+                    ref_cls,
+                    ref_deltas,
+                    gravity_model=model,
+                    pressure_model=pressure_model,
+                    species="dm",
+                    tstep=tstep,
+                    nt=nt,
+                )
+
+            return train_step_base(gravity_model, optimizer, model_loss_fn)
+
+    return train_step
